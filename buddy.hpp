aab03c63364c6f1c0c9cc4178efaecdacb561a99
V01D-NULL <timster23@protonmail.com>
2022-12-26 02:56:03 -0600

    Cleanup at 3am

--- buddy.hpp
+++ buddy.hpp
@@ -9,73 +9,59 @@
 
 namespace {
 static constexpr bool verbose = false;
-}
+static constexpr bool safety_checks = false;
+}  // namespace
 
 enum FillMode : char {
     ZERO = 0,
     NONE = 1  // Don't fill
 };
 
-template <int highest_order>
 class BuddyAllocator {
 public:
     using Order = int;
     using PhysicalAddress = uint64_t *;
 
-    constexpr static Order min_order = 9;
-    constexpr static Order max_order = highest_order - 3;
-
     BuddyAllocator(PhysicalAddress base)
         : base(base) {
-        if constexpr (verbose)
-            std::cout << "min-order: " << min_order << ", max-order: " << max_order << std::endl;
-    }
-
-    PhysicalAddress alloc_order(Order order, FillMode fill = FillMode::NONE) {
-        return alloc(1 << order, fill);
     }
 
     PhysicalAddress alloc(uint64_t order, FillMode fill = FillMode::NONE) {
         int idx_begin, idx_end;
 
-        if (order > freelist.min_order or order < freelist.max_order) {
-            std::cout << "Bad order: " << order << "\n";
-            return nullptr;
+        if constexpr (safety_checks) {
+            if (order > helper.min_order || order < helper.max_order) {
+                std::cout << "Bad order: " << order << "\n";
+                return nullptr;
+            }
         }
 
-        // # Step 1.
-        // # Calculate the range of nodes we need to check.
+        // Step 1.
+        // Calculate the range of nodes we need to check.
         if (order == 0) {
             idx_begin = idx_end = 0;
         } else {
-            idx_begin = freelist.nodes_at_order(order) - 1;
-            idx_end = idx_begin + freelist.nodes_at_order(order) - 1;
+            idx_begin = helper.nodes_at_order(order) - 1;
+            idx_end = idx_begin + helper.nodes_at_order(order) - 1;
         }
 
-        // # Step 2.
-        // # Find a free node at this order.
-        // # Bit position of the node that was allocated (-1 indicates allocation failure)
+        // Step 2.
+        // Find a free node at this order.
+        // Bit position of the node that was allocated (-1 indicates allocation failure)
         int allocated_node_idx = -1;
 
-        // # Calculate the number of bit-shifts to get the left-most node of this order
+        // Calculate the number of bit-shifts to get the left-most node of this order
         int shift = (1 << order) - 1;
-        // std::cout << "begin, end: " << idx_begin << "," << idx_end << '\n';
         for (int x = idx_begin; x <= idx_end; x++) {
-            // std::cout << "test: " << freelist.test_bit(shift) << " (shift: " << shift << ")\n";
-            if (freelist.test_and_set_bit(shift) == 1) {
+            if (helper.test_and_set_bit(shift) == 1) {
                 allocated_node_idx = x;
-                // std::cout << "Allocated node at index " << std::dec << x << " (range: " << idx_begin << "," << idx_end << ")\n";
-                // std::exit(0);
-                // # print(f"Allocated node at index {x} (Range: {idx_begin}-{idx_end})")
-
                 break;
             }
             shift += 1;
         }
 
-
-        // # Step 3.
-        // # Allocate child and parent nodes
+        // Step 3.
+        // Allocate child and parent nodes
         if (allocated_node_idx == -1) {
             std::cout << "Failed to allocate memory at order " << order << '\n';
             return nullptr;
@@ -88,16 +74,16 @@ public:
             allocated_node_idx = 0;
 
         // return (PhysicalAddress)(((uintptr_t)this->base + allocated_node_idx) );
-        return (PhysicalAddress)((uintptr_t)(this->base) + (allocated_node_idx * freelist.order_to_size(order)));
-        // return (BASE_ADDRESS + (allocated_node_idx * freelist.order_to_size(order)));
+        return (PhysicalAddress)((uintptr_t)(this->base) + (allocated_node_idx * helper.order_to_size(order)));
+        // return (BASE_ADDRESS + (allocated_node_idx * helper.order_to_size(order)));
         return nullptr;
     }
 
     bool scan_order(int order) {
         int begin = 0;
-        int end = freelist.nodes_at_order(order);
+        int end = helper.nodes_at_order(order);
         for (int x = begin; x <= end; x++) {
-            if (freelist.test_and_set_bit(x))
+            if (helper.test_and_set_bit(x))
                 return true;
         }
 
@@ -105,58 +91,45 @@ public:
     }
 
     void recursive_allocate_parent(int current_idx) {
-        freelist.set_bit(current_idx);
+        helper.set_bit(current_idx);
         if (current_idx == 0) return;
 
-        recursive_allocate_parent(freelist.parent_of(current_idx));
+        recursive_allocate_parent(helper.parent_of(current_idx));
     }
 
 
     void recursive_allocate_children(int current_idx) {
-        if (current_idx > freelist.last_bit) return;
+        if (current_idx > helper.last_bit) return;
 
-        freelist.set_bit(current_idx);
-        recursive_allocate_children(freelist.left_child(current_idx));
-        recursive_allocate_children(freelist.right_child(current_idx));
+        helper.set_bit(current_idx);
+        recursive_allocate_children(helper.left_child(current_idx));
+        recursive_allocate_children(helper.right_child(current_idx));
     }
 
 
     void free(PhysicalAddress block, Order order) {
-        if (block == nullptr)
-            return;
-
-        order -= 3;
-
-        // There are no buddies at max_order
-        if (order == max_order) {
-            freelist.add(block, max_order - min_order);
-            return;
-        }
-
-        coalesce(block, order);
     }
 
 private:
-    template <typename T, int orders>
-    class Freelist {
+    class Helper {
         long bitmap[1024]{ 0 };
-        static constexpr int BITMAP_BLOCK_SIZE = sizeof(int) * 8;
+        static constexpr int bitmap_block_size = sizeof(int) * 8;
 
     public:
         static constexpr int page_size = 4096;
-        static constexpr int TREE_DEPTH = 9;
+        static constexpr int tree_depth = 9;
 
-        // # Represents the number of bit-shifts required to get the last bit
-        // # (i.e. the right-most node at the bottom of the tree)
-        static constexpr int last_bit = (1 << (TREE_DEPTH + 1)) - 1;
+        // Represents the number of bit-shifts required to get the last bit (aka number of bits in the bitmap)
+        // (i.e. the right-most node at the bottom of the tree)
+        static constexpr int last_bit = (1 << (tree_depth + 1)) - 1;
         static constexpr int first_bit = 0;
 
-        static constexpr int min_order = TREE_DEPTH;
+        static constexpr int min_order = tree_depth;
         static constexpr int max_order = 0;
 
     public:
-        // # Util functions
-        // #
+        // Util functions
+        //
         int order_to_size(int order) {
             return page_size * (1 << (min_order - order));
         }
@@ -166,18 +139,16 @@ private:
         }
 
         bool test_bit(int idx) {
-            return (bitmap[idx / BITMAP_BLOCK_SIZE] & (1 << (idx % BITMAP_BLOCK_SIZE)));
-            // return (bitmap >> idx) & 1;
+            return (bitmap[idx / bitmap_block_size] & (1 << (idx % bitmap_block_size)));
         }
 
         void set_bit(int idx) {
-            bitmap[idx / BITMAP_BLOCK_SIZE] |= (1 << (idx % BITMAP_BLOCK_SIZE));
-            // bitmap |= 1 << idx;
+            bitmap[idx / bitmap_block_size] |= (1 << (idx % bitmap_block_size));
         }
 
-        // # Set bit if clear
-        // # Returns True if the bit could be set.
-        // # If not, it returns False
+        // Set bit if clear
+        // Returns True if the bit could be set.
+        // If not, it returns False
         bool test_and_set_bit(int idx) {
             if (test_bit(idx) == 0) {
                 set_bit(idx);
@@ -192,100 +163,24 @@ private:
             return ((idx - 1) ^ 1) + 1;
         }
 
-
         // Return the parent's bit index of a given bit index
         int parent_of(int idx) {
             return (idx - 1) / 2;
         }
 
-
         int left_child(int idx) {
             return (idx * 2) + 1;
         }
 
-
         int right_child(int idx) {
             return (idx * 2) + 2;
         }
-
-
-        T list[orders + 1]{};  // Todo: Think of a way to allocate enough memory for this array dynamically
-
-    public:
-        void add(const T &block, Order order) {
-            if (block)
-                *(T *)block = list[order];
-
-            list[order] = block;
-        }
-
-        T remove(Order order) {
-            T element = list[order];
-
-            if (element == nullptr)
-                return nullptr;
-
-            list[order] = *(T *)list[order];
-
-            return element;
-        }
-
-        T next(const T &block) const {
-            return *reinterpret_cast<T *>(block);
-        }
-
-        bool find(const T &block, Order order) const {
-            T element = list[order];
-            while (element != nullptr && element != block) {
-                element = next(element);
-            }
-
-            return element != nullptr;
-        }
     };
 
-    static constexpr int log2(int size) {
-        int result = 0;
-        while ((1U << result) < size) {
-            ++result;
-        }
-        return result;
-    }
-
-    inline PhysicalAddress buddy_of(PhysicalAddress block, Order order) {
-        return base + ((block - base) ^ (1 << order));
-    }
-
     void coalesce(PhysicalAddress block, Order order) {
-        // Description:
-        // Try to merge 'block' and it's buddy into one larger block at 'order + 1'
-        // If both block are free, remove them and insert the smaller of
-        // the two blocks into the next highest order and repeat that process.
-
-        if (order == max_order)
-            return;
-
-        PhysicalAddress buddy = buddy_of(block, order);
-        if (buddy == nullptr)
-            return;
-
-        // Scan freelist for the buddy. (This isn't really efficient)
-        // Could be optimized with a bitmap to track the state of each node.
-        // The overhead of a bitmap would be minimal + the allocator itself has little to no overhead so size complexity should not be of concern.
-        bool is_buddy_free = freelist.find(buddy, order - min_order);
-
-        // Buddy block is free, merge them together
-        if (is_buddy_free) {
-            freelist.remove(order);
-            coalesce(std::min(block, buddy), order + 1);  // std::min ensures that the smaller block of memory is merged with a larger and not vice-versa (which wouldn't work)
-        }
-
-        // The buddy is not free and merging is not possible.
-        // Therefore it is simply put back onto the freelist.
-        freelist.add(block, order - min_order);
     }
 
 private:
-    Freelist<PhysicalAddress, max_order - min_order> freelist;
+    Helper helper;
     PhysicalAddress base;
 };
--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int main(void) {
     constexpr auto memory_size = 1 << max_order;
 
     auto base = new uint64_t[1 << max_order];
-    BuddyAllocator<max_order> instance(base);
+    BuddyAllocator instance(base);
 
     std::cout << "Buddy test: Allocating " << pages << " pages which totals " << memory_size << " bytes.\n";
     std::cout << "Base: 0x" << std::hex << (size_t)base << '\n';
@@ -19,14 +19,17 @@ int main(void) {
     // Allocate 1GiB in 4kib chunks for testing purposes
     for (auto i = 0; i < pages; i++) {
         // TODO: Order 1 returns the size of order 0 for some reason... every other order is fine... look into that
-        auto ptr = instance.alloc(1);
+        auto ptr = instance.alloc(9);
         if (!ptr) {
-            std::cout << "bad ptr ig\n";
+            // std::cout << "bad ptr ig\n";
             break;
         }
         // auto ptr = instance.alloc(4096);
-        std::cout << "#" << std::dec << i << ": " << std::hex << ptr << '\n';
+        // std::cout << "#" << std::dec << i << ": " << std::hex << ptr << '\n';
     }
+
+    // Expectedly fails. Allocator is OOM.
+    // std::cout << "Trying again..." << instance.alloc(9) << '\n';
 
     auto elapsed = std::chrono::high_resolution_clock::now() - start;
     auto usec = std::chrono::duration_cast<std::chrono::microseconds>(elapsed);
